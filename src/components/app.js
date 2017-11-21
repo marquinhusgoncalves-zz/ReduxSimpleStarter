2da685d8adec353b6e1571af927f48ce4bb76b9e
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2017-11-21 21:56:06 -0200

    Evolution the component

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './search_bar';
+import VideoList from './video_list';
+const API_KEY = 'AIzaSyB0AHBI5Bf3e1dVMUOSJWZdFQxHAkG9zdY';
+
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
